44aa50d26c2a97537d5c9b181593f3b0c8842002
Devon <Dwill629@gmail.com>
2021-05-01 13:17:21 -0400

    added imports to signUp.js

---
+++ instafilter/src/pages/signup.js
@@ -0,0 +1,5 @@
+import { useState, useContext, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+import * as ROUTES from '../constants/routes';
+import { doesUsernameExist } from '../services/firebase';
