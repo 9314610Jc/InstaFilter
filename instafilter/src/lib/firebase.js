680363acbe495025890567b56e379d344a55ec4e
Stephen Wright <wrightsah@icloud.com>
2021-05-01 11:12:16 -0400

    added config to firebase from firestore

--- instafilter/src/lib/firebase.js
+++ instafilter/src/lib/firebase.js
@@ -4,9 +4,17 @@ import 'firebase/auth';
 
 // import intital seed file
 
-const config = {}; // this needs to come from firebase
+const config = {
+    apiKey: "AIzaSyC24ut1eS0xHiCbG7cUQ3xxjQMOsPErJ0s",
+    authDomain: "insta-filter-187fd.firebaseapp.com",
+    projectId: "insta-filter-187fd",
+    storageBucket: "insta-filter-187fd.appspot.com",
+    messagingSenderId: "204493802530",
+    appId: "1:204493802530:web:e5cc225548f1499d980e7b",
+    measurementId: "G-8RTVG51YWV"
+}; // this needs to come from firebase
 
-const firebase = Firebase.initializeApp;
+const firebase = Firebase.initializeApp(config);
 
 //
 
