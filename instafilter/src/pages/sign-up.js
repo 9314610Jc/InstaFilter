c97f87e4663fa103a95d3b1fc2c34439bd30f78d
Devon <Dwill629@gmail.com>
2021-05-01 13:25:12 -0400

    added const handleSignUp

--- instafilter/src/pages/sign-up.js
+++ instafilter/src/pages/sign-up.js
@@ -16,3 +16,45 @@ export default function SignUp() {
     const [error, setError] = useState('');
     const isInvalid = password === '' || emailAddress === '';
   
+    const handleSignUp = async (event) => {
+        event.preventDefault();
+    
+        const usernameExists = await doesUsernameExist(username);
+        if (!usernameExists) {
+          try {
+            const createdUserResult = await firebase
+              .auth()
+              .createUserWithEmailAndPassword(emailAddress, password);
+    
+            // authentication
+            // -> emailAddress & password & username (displayName)
+            await createdUserResult.user.updateProfile({
+              displayName: username
+            });
+    
+            // firebase user collection (create a document)
+            await firebase
+              .firestore()
+              .collection('users')
+              .add({
+                userId: createdUserResult.user.uid,
+                username: username.toLowerCase(),
+                fullName,
+                emailAddress: emailAddress.toLowerCase(),
+                following: ['2'],
+                followers: [],
+                dateCreated: Date.now()
+              });
+    
+            history.push(ROUTES.DASHBOARD);
+          } catch (error) {
+            setFullName('');
+            setEmailAddress('');
+            setPassword('');
+            setError(error.message);
+          }
+        } else {
+          setUsername('');
+          setError('That username is already taken, please try another.');
+        }
+      };
