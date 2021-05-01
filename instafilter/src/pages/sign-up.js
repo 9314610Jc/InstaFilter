c67ebeaea4bd439dead44b22ddf8457d15a916d0
Devon <Dwill629@gmail.com>
2021-05-01 13:22:29 -0400

    added export default function signup pt1

--- instafilter/src/pages/sign-up.js
+++ instafilter/src/pages/sign-up.js
@@ -3,3 +3,16 @@ import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
 import { doesUsernameExist } from '../services/firebase';
+
+export default function SignUp() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+  
+    const [username, setUsername] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const [error, setError] = useState('');
+    const isInvalid = password === '' || emailAddress === '';
+  
