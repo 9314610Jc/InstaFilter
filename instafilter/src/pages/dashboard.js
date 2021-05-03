c95a744196060f79e95562e86ff788601e061218
Devon <Dwill629@gmail.com>
2021-05-03 19:27:37 -0400

    added return for dashboard

--- instafilter/src/pages/dashboard.js
+++ instafilter/src/pages/dashboard.js
@@ -11,3 +11,16 @@ export default function Dashboard({ user: loggedInUser }) {
   useEffect(() => {
     document.title = 'Instagram';
   }, []);
+
+  return (
+    <LoggedInUserContext.Provider value={{ user, setActiveUser }}>
+      <div className="bg-gray-background">
+        <Header />
+        <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+          <Timeline />
+          <Sidebar />
+        </div>
+      </div>
+    </LoggedInUserContext.Provider>
+  );
+}
