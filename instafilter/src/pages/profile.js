0cdfe8c72da046c44c151b502951cdd980641923
Devon <Dwill629@gmail.com>
2021-05-03 19:50:29 -0400

    added profile imports

---
+++ instafilter/src/pages/profile.js
@@ -0,0 +1,6 @@
+import { useParams, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getUserByUsername } from '../services/firebase';
+import * as ROUTES from '../constants/routes';
+import Header from '../components/header';
+import UserProfile from '../components/profile';
