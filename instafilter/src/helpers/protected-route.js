1794245ada5139a27f761abd6ae5ba05db747123
Stephen Wright <wrightsah@icloud.com>
2021-05-03 19:18:39 -0400

    set route dependencies

---
+++ instafilter/src/helpers/protected-route.js
@@ -0,0 +1,8 @@
+// dependencies
+
+import React from 'react';
+import PropTyples from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
+
+
