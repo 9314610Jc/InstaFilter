65bee66359158cd05e1cc93de8cc83ec8912d863
josh c <jlc42302@gmail.com>
2021-05-03 19:36:34 -0400

    props

--- instafilter/src/components/post/action.js
+++ instafilter/src/components/post/action.js
@@ -82,3 +82,10 @@ export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }) 
       </>
     );
   }
+
+  Actions.propTypes = {
+    docId: PropTypes.string.isRequired,
+    totalLikes: PropTypes.number.isRequired,
+    likedPhoto: PropTypes.bool.isRequired,
+    handleFocus: PropTypes.func.isRequired
+  };
