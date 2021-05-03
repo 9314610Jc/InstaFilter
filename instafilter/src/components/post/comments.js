f640ad2608d7f90481aba92807101eca2759674d
josh c <jlc42302@gmail.com>
2021-05-03 19:46:47 -0400

    imports

---
+++ instafilter/src/components/post/comments.js
@@ -0,0 +1,5 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { formatDistance } from 'date-fns';
+import { Link } from 'react-router-dom';
+import AddComment from './add-comment';
