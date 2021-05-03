0248ebdf1656178716da5328c075fd286138b754
josh c <jlc42302@gmail.com>
2021-05-03 19:56:41 -0400

    imports

---
+++ instafilter/src/components/post/index.js
@@ -0,0 +1,7 @@
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+import Header from './header';
+import Image from './image';
+import Actions from './actions';
+import Footer from './footer';
+import Comments from './comments';
