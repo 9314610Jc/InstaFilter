5586a03226783667d6894155d93a9591f738fd80
josh c <jlc42302@gmail.com>
2021-05-03 19:07:39 -0400

    does username exist

--- instafilter/src/services/firebase.js
+++ instafilter/src/services/firebase.js
@@ -1,4 +1,16 @@
 import { firebase, FieldValue } from '../lib/firebase';
+
+export async function doesUsernameExist(username) {
+  const result = await firebase
+    .firestore()
+    .collection('users')
+    .where('username', '==', username)
+    .get();
+
+  return result.docs.length > 0;
+}
+
+
 // get user from the firestore where userId === userId (passed from the auth)
 export async function getUserByUserId(userId) {
     const result = await firebase.firestore().collection('users').where('userId', '==', userId).get();
