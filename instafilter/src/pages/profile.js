1fb86083ccba58459a8f683154481324bc1a3b80
Devon <Dwill629@gmail.com>
2021-05-03 19:51:40 -0400

    added export username, history const

--- instafilter/src/pages/profile.js
+++ instafilter/src/pages/profile.js
@@ -4,3 +4,8 @@ import { getUserByUsername } from '../services/firebase';
 import * as ROUTES from '../constants/routes';
 import Header from '../components/header';
 import UserProfile from '../components/profile';
+
+export default function Profile() {
+  const { username } = useParams();
+  const [user, setUser] = useState(null);
+  const history = useHistory();
