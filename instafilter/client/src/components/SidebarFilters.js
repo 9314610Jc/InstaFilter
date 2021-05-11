182582d04586bd577bb0af9e49b4d5e0abf60b27
Stephen Wright <wrightsah@icloud.com>
2021-05-10 21:25:20 -0400

    added icons for up and down arrows

---
+++ instafilter/client/src/components/SidebarFilters.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from 'styled-components';
+
+// import links for pages
+import { Link } from 'react-router-dom';
+
+const SidebarFilters = () => {
+  return (
+    <div>
+      Sidebar
+    </div>
+  );
+};
+
+export default SidebarFilters;
---
+++ instafilter/client/src/lib/samplefilters.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import icons
+import * as RiIcons from 'react-icons/ri';
+
+export const Filters = [
+    {
+        title: 'My Filters', 
+        path: '/myfilters', 
+        // add icons for open and closed
+        iconClosed: <RiIcons.RiArrowDownFill />,
+        iconOpen: <RiIcons.RiArrowUpFill />
+    }, 
+    
+]
