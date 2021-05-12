5c1c768b58e3dc6bb2301a6c80efeac9602b0fbc
Stephen Wright <wrightsah@icloud.com>
2021-05-12 19:24:17 -0400

    fixed sidenav bugs

---
+++ instafilter/client/src/components/SidebarData.js
@@ -0,0 +1,54 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as RiIcons from "react-icons/ri";
+
+export const SidebarData = [
+  {
+    title: "My Filters",
+    // path: "/myfilters",
+  },
+  {
+    title: 'My Filters',
+    icon: <AiIcons.AiFillHome />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+    // filters get pushed here
+    subNav: [{
+      title: '(Sample Filter) Family & Friends',
+      users: []
+    }, {}],
+  },
+  {
+    title: "Team",
+    path: "/team",
+    icon: <IoIcons.IoMdPeople />,
+  },
+  {
+    title: "Messages",
+    path: "/messages",
+    icon: <FaIcons.FaEnvelopeOpenText />,
+
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+
+    subNav: [
+      {
+        title: "Message 1",
+        path: "/messages/message1",
+        icon: <IoIcons.IoIosPaper />,
+      },
+      {
+        title: "Message 2",
+        path: "/messages/message2",
+        icon: <IoIcons.IoIosPaper />,
+      },
+    ],
+  },
+  {
+    title: "Support",
+    path: "/support",
+    icon: <IoIcons.IoMdHelpCircle />,
+  },
+];
